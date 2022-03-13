3377e61e9ab30c374874137c5928ddf6c746f2af
angelicakaye <74851842+angelicakaye@users.noreply.github.com>
2022-03-14 04:08:05 +0800

    pdt cart & pdt deets w/ linking to pages

--- EShop/Template/_allproductsPdtDeets.php
+++ EShop/Template/_allproductsPdtDeets.php
@@ -9,27 +9,14 @@
   <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,600;0,800;1,400;1,600;1,800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-
 <body>
 
   <!--- All Cakes --->
   <div class="small-container">
-    <div class="row row-2">
-      <h2 class="title">Our Products</h2>
-      <select>
-        <option>Default Sorting</option>
-        <option>Sort by price</option>
-        <option>Simple Cakes</option>
-        <option>Floral Cakes</option>
-        <option>Galaxy Cakes</option>
-        <option>Local Cakes</option>
-        <option>Fruity Cakes</option>
-      </select>
-    </div>
-    
+    <h2 class="title">Our Products</h2>
     <div class="row">
       <div class="col-4">
-          <img src="1.jpeg">
+          <a href="productDetails.html"><img src="1.jpeg"></a>
           <h4>Cherry Blossom</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
@@ -41,7 +28,7 @@
           <p>$45</p>
       </div>
       <div class="col-4">
-          <img src="2.jpeg">
+          <a href="productDetails.html"><img src="2.jpeg"></a>
           <h4>Ombre Rosette</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
@@ -53,7 +40,7 @@
           <p>$50</p>
       </div>
       <div class="col-4">
-          <img src="3.jpeg">
+          <a href="productDetails.html"><img src="3.jpeg"></a>
           <h4>Rustic Nature</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
@@ -65,7 +52,7 @@
           <p>$55</p>
       </div>
       <div class="col-4">
-          <img src="4.jpeg">
+          <a href="productDetails.html"><img src="4.jpeg"></a>
           <h4>Macaron</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
@@ -81,7 +68,7 @@
     <!--- 2nd row --->
     <div class="row">
       <div class="col-4">
-          <img src="5.jpeg">
+          <a href="productDetails.html"><img src="5.jpeg"></a>
           <h4>Astronaut Explorer</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
@@ -93,7 +80,7 @@
           <p>$85</p>
       </div>
       <div class="col-4">
-          <img src="6.jpeg">
+          <a href="productDetails.html"><img src="6.jpeg"></a>
           <h4>Majestic Galaxy</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
@@ -105,7 +92,7 @@
           <p>$75</p>
       </div>
       <div class="col-4">
-          <img src="7.jpeg">
+          <a href="productDetails.html"><img src="7.jpeg"></a>
           <h4>Mahjong Extraordinaire</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
@@ -117,7 +104,7 @@
           <p>$120</p>
       </div>
       <div class="col-4">
-          <img src="8.jpeg">
+          <a href="productDetails.html"><img src="8.jpeg"></a>
           <h4>Bubble Tea</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
@@ -133,7 +120,7 @@
     <!--- 3rd row --->
     <div class="row">
       <div class="col-4">
-          <img src="9.jpeg">
+          <a href="productDetails.html"><img src="9.jpeg"></a>
           <h4>Fruity Galore</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
@@ -145,8 +132,8 @@
           <p>$45</p>
       </div>
       <div class="col-4">
-          <img src="10.jpeg">
-          <h4>New 2</h4>
+          <a href="productDetails.html"><img src="10.jpeg"></a>
+          <h4>Strawberry Shortcake</h4>
           <div class="rating">
             <i class="fa fa-star"></i>
             <i class="fa fa-star"></i>
--- EShop/header_final.php
+++ EShop/header_final.php
@@ -8,7 +8,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,600;0,800;1,400;1,600;1,800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  
+
   <?php
     //require functions_final.php file
     require('functions_final.php');
@@ -26,12 +26,12 @@
         </div>
         <nav>
           <ul>
-            <li><a href-"">Home</a></li>
-            <li><a href-"">Our Cakes</a></li>
-            <li><a href-"">Reviews</a></li>
+            <li><a href-"main page link where ah">Home</a></li>
+            <li><a href-"_allproducts.php">Our Cakes</a></li>
+            <li><a href-"main page link where ah">Reviews</a></li>
           </ul>
         </nav>
-        <img src="img/others/cart.png" width="30px" height="30px">
+        <a href="cart.html>"<img src="img/others/cart.png" width="30px" height="30px"></a>
       </div>
     </div>
   </div>
