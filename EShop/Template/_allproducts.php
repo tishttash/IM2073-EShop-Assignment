df60edb96724c558c837ff1c94eebb6c0c3c36cd
Tasha <tashaasng78ws@gmail.com>
2022-03-15 04:29:27 +0800

    tried to integrate filter into php file... KEYWORD: TRIED :(

--- EShop/Template/_allproducts.php
+++ EShop/Template/_allproducts.php
@@ -25,8 +25,8 @@
     <div class="filter-row">
       <div class="filters">
         <ul id="filter-list">
-          <li class="filter active" data-filter="all">All Cakes</li>
-          <li class="filter" data-filter="Floral">Floral Cakes</li>
+          <li class="filter" data-filter="all">All Cakes</li>
+          <li class="filter active" data-filter="Floral">Floral Cakes</li>
           <li class="filter" data-filter="Simple">Simple Cakes</li>
           <li class="filter" data-filter="Galaxy">Galaxy Cakes</li>
           <li class="filter" data-filter="Local">Local Cakes</li>
@@ -39,28 +39,28 @@
       <ul class="all-cakes">
         <div class="row">
           <?php foreach($product_shuffle as $item) { ?>
-          <div class="col-4" data-category="can import the type of cake here? ie floral, etc">
-          <a href="<?php printf('%s?id=%s','product_final.php',$item['id']);?>"><img src="<?php echo $item['image']??"./img/pdt/1.jpeg";?>"></a>
-          <h4><?php echo $item['name']??"Unknown";?></h4>
-          <div class="rating">
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-            <i class="fa fa-star"></i>
-          </div>
-          <p><?php echo $item['cost']??"0";?></p>
-          </div>
+            <div class="col-4" data-category="<?php echo $item['type'];?>">
+              <a href="<?php printf('%s?id=%s','product_final.php',$item['id']);?>"><img src="<?php echo $item['image']??"./img/pdt/1.jpeg";?>"></a>
+              <h4><?php echo $item['name']??"Unknown";?></h4>
+              <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+              </div>
+              <p><?php echo $item['cost']??"0";?></p>
+            </div>
           <?php } //close foreach function ?>
         </div>
       </ul>
     </div>
   </div>
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/jquery.filterizr.min.js"></script>
-  <script src="js/script.js"></script>
+  <script src="./js/jquery-3.3.1.min.js"></script>
+  <script src="./js/popper.min.js"></script>
+  <script src="./js/jquery.filterizr.min.js"></script>
+  <script src="./js/script.js"></script>
 
 </body>
 </html>
